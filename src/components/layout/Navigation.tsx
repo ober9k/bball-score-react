169d160136f3e91ea7e7dc183dc08e836f434364
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-01 23:15:42 +1100

    Added keys for navigation links

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,11 +1,12 @@
 import { Link } from "@tanstack/react-router";
 import { Paths } from "../../routes/paths.ts";
 
-const links: Array<{ title: string, to: string }> = [
-  { title: "Home", to: Paths.Home, },
-  { title: "Games", to: Paths.Games, },
-  { title: "Players", to: Paths.Players, },
-  { title: "Teams", to: Paths.Teams, },
+let linkId = 1;
+const links: Array<{ id: number, title: string, to: string }> = [
+  { id: linkId++, title: "Home", to: Paths.Home, },
+  { id: linkId++, title: "Games", to: Paths.Games, },
+  { id: linkId++, title: "Players", to: Paths.Players, },
+  { id: linkId++, title: "Teams", to: Paths.Teams, },
 ];
 
 export default function Navigation() {
@@ -19,7 +20,7 @@ export default function Navigation() {
         </div>
         <div className="">
           {links.map((link) => (
-            <Link to={link.to} className="[&.active]:font-bold rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
+            <Link key={link.id} to={link.to} className="[&.active]:font-bold rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
               {link.title}
             </Link>
           ))}
