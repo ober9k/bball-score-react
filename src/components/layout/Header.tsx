03b8a63532e342ef74045b8b7bdb3aa540d4b5e5
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-06 16:43:02 +1100

    Added component for page header and a light background

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react";
+
+type HeaderProps = {
+  children?: ReactNode,
+}
+
+export default function Header(props: HeaderProps) {
+  return (
+    <header className="relative bg-white shadow-sm">
+      <div className="mx-auto max-w-2xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-medium tracking-tight text-gray-900 text-left">
+          {props.children}
+        </h1>
+      </div>
+    </header>
+  );
+}
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Header from "../components/layout/Header.tsx";
 import Navigation from "../components/layout/Navigation.tsx";
 
 export default function RootLayout() {
   return (
     <>
-      <Navigation />
+      <Navigation/>
+      <Header>
+        %title%
+      </Header>
       <Outlet/>
       <TanStackRouterDevtools/>
     </>
