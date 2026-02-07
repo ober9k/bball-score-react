8020221ddc184e016ecfa9bfb9555c4980ead27e
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-08 01:19:58 +1100

    Added statistics link to navigation

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -3,10 +3,10 @@ import { Paths } from "../../routes/paths.ts";
 
 let linkId = 1;
 const links: Array<{ id: number, title: string, to: string }> = [
-  { id: linkId++, title: "Home", to: Paths.Home, },
   { id: linkId++, title: "Games", to: Paths.Games, },
   { id: linkId++, title: "Players", to: Paths.Players, },
   { id: linkId++, title: "Standings", to: Paths.Standings, },
+  { id: linkId++, title: "Statistics", to: Paths.Statistics, },
   { id: linkId++, title: "Teams", to: Paths.Teams, },
 ];
 
@@ -14,14 +14,16 @@ export default function Navigation() {
   return (
     <div className="bg-gray-800 flex h-16 items-center justify-between">
       <div className="flex items-center">
-        <div className="shrink-0 pl-4 pr-2 py-2">
+        <div className="shrink-0 pl-4 pr-4 py-2">
           <span className="text-2xl grayscale">
+            <Link to={Paths.Home}>
             🏀
+            </Link>
           </span>
         </div>
         <div className="">
           {links.map((link) => (
-            <Link key={link.id} to={link.to} className="[&.active]:font-bold rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
+            <Link key={link.id} to={link.to} className="[&.active]:font-bold rounded-md px-2 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
               {link.title}
             </Link>
           ))}
