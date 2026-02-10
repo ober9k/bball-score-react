94d2a6bbb2fe70b069b36838f51af52bb2135232
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-10 22:36:47 +1100

    Added cors to express server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"] // todo: update to env
+}
+
+app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
     res.json({
@@ -7,6 +13,7 @@ app.get("/api", (req, res) => {
     });
 });
 
+// todo: update to env
 app.listen(8080, () => {
     console.log("Server started on port 8080");
 });
