daaa9465269acd5f5df334780d51e91bf524106f
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-12 01:09:08 +1100

    Updated update team to use API (initial wire-up)

--- src/data/actions.ts
+++ src/data/actions.ts
@@ -1,18 +1,10 @@
+import axios from "axios";
 import type { Team } from "../types/Team.ts";
-import { findTeamById } from "./teams.ts";
 
 export type TeamData = Pick<Team, "id" | "name" | "teamStyle">;
 
 export async function updateTeam(teamData: TeamData) {
-  console.log("Updating Team:", teamData);
-
-  const existingTeam = findTeamById(teamData.id);
-
-  /* keeping it simple for now */
-  existingTeam.name = teamData.name;
-  existingTeam.teamStyle.bgColor = teamData.teamStyle.bgColor;
-  existingTeam.teamStyle.textColor = teamData.teamStyle.textColor;
-
-  await new Promise((res) => setTimeout(res, 1000));
+  // TODO: tidy up and wire-up the remaining logic
+  await axios.put("http://localhost:8080/api/teams/" + teamData.id, teamData);
   return "Updated";
 }
