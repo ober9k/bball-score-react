a1640090ec2b1ec95618a9478996bb55ca1b7bde
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-11 12:29:56 +1100

    Added basic loading page

---
+++ src/pages/other/LoadingPage.tsx
@@ -0,0 +1,17 @@
+import Content from "../../components/layout/page/Content.tsx";
+import Header from "../../components/layout/page/Header.tsx";
+
+export default function LoadingPage() {
+  return (
+    <>
+      <Header>
+        Loading...
+      </Header>
+      <Content>
+        <p>
+          Loading...
+        </p>
+      </Content>
+    </>
+  )
+}
