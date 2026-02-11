e7c984bf5dc89781eb57c3fffe65012c6786b876
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-11 12:03:36 +1100

    Updated players and teams endpoints to use mock data

--- server/src/controllers/playerController.ts
+++ server/src/controllers/playerController.ts
@@ -1,10 +1,12 @@
 import { type Request, type Response } from "express";
+import { mockPlayers } from "../../../src/data/players";
 import { sleep } from "../utils/sleep";
 
 export async function getPlayers(req: Request, res: Response) {
     await sleep(1000);
 
     res.status(200).json({
-       players: [],
+      /* referencing mock data for now */
+       players: mockPlayers,
     });
 }
--- server/src/controllers/teamController.ts
+++ server/src/controllers/teamController.ts
@@ -1,10 +1,12 @@
 import { type Request, type Response } from "express";
+import { mockTeams } from "../../../src/data/teams";
 import { sleep } from "../utils/sleep";
 
 export async function getTeams(req: Request, res: Response) {
     await sleep(1000);
 
     res.status(200).json({
-        teams: [],
+        /* referencing mock data for now */
+        teams: mockTeams,
     });
 }
