ec5bdc5ef125b581fbfc0a0e7dd2a7ec794abe03
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-13 15:57:02 +1100

    Added basic error handler

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
+import { isHttpException } from "@models/http-exception.model";
+import type HttpException from "@models/http-exception.model";
 import homeRoutes from "@routes/homeRoutes";
 import playerRoutes from "@routes/playerRoutes";
 import standingsRoutes from "@routes/standingsRoutes";
 import teamRoutes from "@routes/teamRoutes";
 import cors from "cors";
-import express from "express";
+import express, { type NextFunction, type Request, type Response } from "express";
+
 
 const app = express();
 
@@ -17,5 +20,27 @@ app.use(homeRoutes);
 app.use(playerRoutes);
 app.use(standingsRoutes);
 app.use(teamRoutes);
+
+app.use((
+  err: Error | HttpException,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (isHttpException(err)) {
+    // work with the custom handler
+    switch (err.errorCode) {
+      case 404:
+        res.status(404).json(err.message);
+        return;
+      default:
+        res.status(500).json(err.message);
+    }
+    return;
+  }
+
+  // default fall back handling
+  res.status(500).json(err.message);
+});
 
 export default app;
