e20796a4b6dc7b42eb15f3aec8bc30b2c18eb372
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-15 00:23:07 +1100

    Corrected IDs for teams with new mock data

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,13 +1,13 @@
 import type { Team } from "../types/Team.ts";
 
 export const mockTeams: Array<Team> = [
-  { id: 10, name: "SayLess 2.0", shortName: "SAY", teamStyle: { bgColor: "#F34841", textColor: "#ffffff" } },
-  { id: 11, name: "Bloodlines", shortName: "BLD", teamStyle: { bgColor: "#dfdfdf", textColor: "#000000" } },
-  { id: 12, name: "Chaos", shortName: "CHA", teamStyle: { bgColor: "#B72E2F", textColor: "#ffffff" } },
-  { id: 13, name: "eHub Knights 2.0", shortName: "EHB", teamStyle: { bgColor: "#27A2CC", textColor: "#0B172D" } },
-  { id: 14, name: "FBBL", shortName: "FBB", teamStyle: { bgColor: "#6D591F", textColor: "#ffffff" } },
-  { id: 15, name: "Hustlers", shortName: "HUS", teamStyle: { bgColor: "#C1633A", textColor: "#ffffff" } },
-  { id: 16, name: "InvestorKit", shortName: "INV", teamStyle: { bgColor: "#58B484", textColor: "#ffffff" } },
+  { id: 11, name: "SayLess 2.0", shortName: "SAY", teamStyle: { bgColor: "#F34841", textColor: "#ffffff" } },
+  { id: 12, name: "Bloodlines", shortName: "BLD", teamStyle: { bgColor: "#dfdfdf", textColor: "#000000" } },
+  { id: 13, name: "Chaos", shortName: "CHA", teamStyle: { bgColor: "#B72E2F", textColor: "#ffffff" } },
+  { id: 14, name: "eHub Knights 2.0", shortName: "EHB", teamStyle: { bgColor: "#27A2CC", textColor: "#0B172D" } },
+  { id: 15, name: "FBBL", shortName: "FBB", teamStyle: { bgColor: "#6D591F", textColor: "#ffffff" } },
+  { id: 16, name: "Hustlers", shortName: "HUS", teamStyle: { bgColor: "#C1633A", textColor: "#ffffff" } },
+  { id: 17, name: "InvestorKit", shortName: "INV", teamStyle: { bgColor: "#58B484", textColor: "#ffffff" } },
 ];
 
 /**
