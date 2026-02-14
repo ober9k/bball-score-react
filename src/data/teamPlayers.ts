8bda958533e72eb8054eb32c7f5d077c09131067
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-15 00:48:37 +1100

    Corrected inconsistency with mock team player data

--- src/data/teamPlayers.ts
+++ src/data/teamPlayers.ts
@@ -1,18 +1,14 @@
-import type { Player } from "../types/Player.ts";
-import type { Team } from "../types/Team.ts";
 import type { TeamPlayer } from "../types/TeamPlayer.ts";
-import { mockPlayers } from "./players.ts";
-import { tempFindTeamById } from "./teams.ts";
 
 export const mockTeamPlayers: Array<TeamPlayer> = [
-  { teamId: 10, playerId: 11 },
-  { teamId: 10, playerId: 12 },
-  { teamId: 10, playerId: 13 },
-  { teamId: 10, playerId: 14 },
-  { teamId: 10, playerId: 15 },
-  { teamId: 11, playerId: 21 },
-  { teamId: 11, playerId: 22 },
-  { teamId: 11, playerId: 23 },
-  { teamId: 11, playerId: 24 },
-  { teamId: 11, playerId: 25 },
+  { teamId: 11, playerId: 11 },
+  { teamId: 11, playerId: 12 },
+  { teamId: 11, playerId: 13 },
+  { teamId: 11, playerId: 14 },
+  { teamId: 11, playerId: 15 },
+  { teamId: 11, playerId: 16 },
+  { teamId: 11, playerId: 17 },
+  { teamId: 11, playerId: 18 },
+  { teamId: 11, playerId: 19 },
+  { teamId: 11, playerId: 20 },
 ];
