c008f8cb8de819e04aec2b4bbc25160802c1641e
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-15 01:24:29 +1100

    Added handling to show empty players list

--- src/components/PlayersList.tsx
+++ src/components/PlayersList.tsx
@@ -6,26 +6,43 @@ type PlayerListProps = {
   players: Array<Player>,
 }
 
-export default function PlayersList({ players }: PlayerListProps) {
+function NonEmptyPlayersList({ players }: PlayerListProps) {
   return (
-    <div>
-      <div className="grid grid-cols-3 gap-4">
-        {players.map((player) => (
-          <div key={player.id} className="border border-gray-200 rounded-sm">
-            <div className="relative min-h-24 bg-gray-300">
-              <div className="absolute right-0">
-                <img src="/images/player.png" alt={player.name} height={96} width={96} className={"mx-1"} />
-              </div>
-              <div className="absolute p-2">
-                <strong className="font-medium text-lg"><PlayerLink player={player}/></strong>
-              </div>
+    <div className="grid grid-cols-3 gap-4">
+      {players.map((player) => (
+        <div key={player.id} className="border border-gray-200 rounded-sm">
+          <div className="relative min-h-24 bg-gray-300">
+            <div className="absolute right-0">
+              <img src="/images/player.png" alt={player.name} height={96} width={96} className={"mx-1"}/>
             </div>
-            <div className="p-1">
-              #{player.number} &ndash; {mapBasePosition(player.position)}
+            <div className="absolute p-2">
+              <strong className="font-medium text-lg"><PlayerLink player={player}/></strong>
             </div>
           </div>
-        ))}
-      </div>
+          <div className="p-1">
+            #{player.number} &ndash; {mapBasePosition(player.position)}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function EmptyPlayersList() {
+  return (
+    <p className="w-full text-center">
+      No active players to show for the current season.
+    </p>
+  )
+}
+
+export default function PlayersList({ players }: PlayerListProps) {
+  const hasPlayers = !!players.length;
+
+  return (
+    <div>
+      {hasPlayers && <NonEmptyPlayersList players={players} />}
+      {!hasPlayers && <EmptyPlayersList/>}
     </div>
   );
 }
