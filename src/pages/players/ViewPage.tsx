bf8613e1d3b478a43f5a90bc8f3f5ff617d9bff6
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-15 01:25:44 +1100

    Added possibly redundant headings to player view

--- src/pages/players/ViewPage.tsx
+++ src/pages/players/ViewPage.tsx
@@ -1,6 +1,7 @@
 import { getRouteApi } from "@tanstack/react-router";
 import Content from "../../components/layout/page/Content.tsx";
 import Header from "../../components/layout/page/Header.tsx";
+import SubHeading from "../../components/layout/page/SubHeading.tsx";
 import AveragesTable from "../../components/stats/AveragesTable.tsx";
 import GameLogTable from "../../components/stats/GameLogTable.tsx";
 import TotalsTable from "../../components/stats/TotalsTable.tsx";
@@ -27,8 +28,17 @@ export default function ViewPage() {
           Player: <strong>{player.name}</strong><br/>
           Team: <strong>{team.name}</strong><br/>
         </p>
+        <SubHeading>
+          Current Season
+        </SubHeading>
         <GameLogTable team={team} gameLogs={gameLogs} />
+        <SubHeading>
+          Player Averages
+        </SubHeading>
         <AveragesTable team={team} gameLogs={gameLogs} />
+        <SubHeading>
+          Player Totals
+        </SubHeading>
         <TotalsTable team={team} gameLogs={gameLogs} />
       </Content>
     </>
