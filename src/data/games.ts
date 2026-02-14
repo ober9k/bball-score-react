445a40fe8d27ae604d22aaa586f54560afddc1c5
Dean Hardaker <ober9k@users.noreply.github.com>
2026-02-15 00:23:26 +1100

    Updated mock games data for a more realistic dataset

--- src/data/games.ts
+++ src/data/games.ts
@@ -9,100 +9,249 @@ import { findTeamById } from "./teams.ts";
 
 let playerLogId = 1;
 
+let gameId = 1;
+let teamLogId = 1;
+
+const gameIds = {
+  W1:  gameId++,
+  W2:  gameId++,
+  W3:  gameId++,
+  W4:  gameId++,
+  W5:  gameId++,
+  W6:  gameId++,
+  W7:  gameId++,
+  W8:  gameId++,
+  W9:  gameId++,
+  W10: gameId++,
+  W11: gameId++,
+  W12: gameId++, // semi-finals
+};
+
 function mockPlayer(playerId: number, played: boolean, points: number, rebounds: number, assists: number, steals: number, blocks: number, turnovers: number, personalFouls: number) {
   return { id: playerLogId++, playerId, played, points, rebounds, assists, steals, blocks, turnovers, personalFouls } as PlayerLog;
 }
 
 export const mockGames: Array<Game> = [
   {
-    id: 1,
-    date: new Date("2025-02-01"),
-    teamLogs: [
-      {
-        id: 1,
-        teamId: 10,
-        teamScore: 55,
-        playerLogs: [
-          mockPlayer(11, true, 15,  7,  3, 1, 0, 2, 0),
-          mockPlayer(12, true, 11,  6,  2, 1, 1, 1, 3),
-          mockPlayer(13, true,  8, 13,  5, 2, 2, 0, 0),
-          mockPlayer(14, true,  5,  3,  2, 1, 0, 3, 2),
-          mockPlayer(15, true, 16,  9,  5, 3, 0, 2, 2),
-        ],
-      },
-      {
-        id: 2,
-        teamId: 11,
-        teamScore: 53,
-        playerLogs: [
-          mockPlayer(21, true, 16,  8,  4, 2, 0, 0, 4),
-          mockPlayer(22, true, 11,  7,  1, 2, 2, 3, 0),
-          mockPlayer(23, true,  7,  2,  3, 1, 1, 4, 1),
-          mockPlayer(24, true, 13,  5,  3, 1, 2, 2, 1),
-          mockPlayer(25, true,  6, 11,  4, 0, 0, 1, 3),
-        ],
-      },
-    ],
+    id: gameIds.W1,
+    date: new Date("2025-07-27"),
+    round: 1,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 48,
+      /*periodScores: [9, 14, 13, 12],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 12,
+      team: undefined,
+      teamScore: 50, // W?
+      // periodScores: [14, 13, 12, 11],
+      playerLogs: [],
+    }],
   },
   {
-    id: 2,
-    date: new Date("2025-02-08"),
-    teamLogs: [
-      {
-        id: 3,
-        teamId: 11,
-        teamScore: 83,
-        playerLogs: [
-          mockPlayer(21, true, 15,  8,  9, 2, 0, 1, 3),
-          mockPlayer(22, true, 14,  9,  1, 0, 2, 4, 0),
-          mockPlayer(23, true,  7, 11,  5, 2, 0, 1, 2),
-          mockPlayer(24, true, 41,  5,  5, 1, 1, 1, 2),
-          mockPlayer(25, true,  6,  6,  4, 0, 5, 2, 2),
-        ],
-      },
-      {
-        id: 4,
-        teamId: 10,
-        teamScore: 67,
-        playerLogs: [
-          mockPlayer(11, true,  9,  5,  1, 2, 0, 2, 0),
-          mockPlayer(12, true, 15,  9,  4, 3, 1, 2, 2),
-          mockPlayer(13, true, 11, 10, 11, 5, 5, 1, 3),
-          mockPlayer(14, true,  7,  9,  1, 1, 0, 3, 2),
-          mockPlayer(15, true, 25,  5,  0, 0, 1, 0, 0),
-        ],
-      },
-    ],
+    id: gameIds.W2,
+    date: new Date("2025-08-03"),
+    round: 2,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 45,
+      /*periodScores: [9, 16, 9, 11],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 16,
+      team: undefined,
+      teamScore: 45, // D?
+      /*periodScores: [9, 13, 12, 11],*/
+      playerLogs: [],
+    }],
   },
   {
-    id: 3,
-    date: new Date("2025-02-15"),
-    teamLogs: [
-      {
-        id: 4,
-        teamId: 10,
-        teamScore: 61,
-        playerLogs: [
-          mockPlayer(11, true, 17,  3,  2, 2, 2, 1, 2),
-          mockPlayer(12, true,  6,  5,  5, 3, 2, 3, 0),
-          mockPlayer(13, true,  8, 16,  2, 1, 1, 3, 3),
-          mockPlayer(14, true,  9,  3,  2, 0, 1, 2, 1),
-          mockPlayer(15, true, 21,  8,  4, 2, 0, 0, 1),
-        ],
-      },
-      {
-        id: 5,
-        teamId: 11,
-        teamScore: 49,
-        playerLogs: [
-          mockPlayer(21, true, 12,  8,  4, 2, 0, 2, 1),
-          mockPlayer(22, true, 15,  7,  0, 1, 0, 2, 0),
-          mockPlayer(23, true,  6,  2,  5, 3, 2, 3, 4),
-          mockPlayer(24, true,  9,  5,  2, 1, 1, 2, 2),
-          mockPlayer(25, true,  7, 10,  3, 1, 1, 0, 1),
-        ],
-      },
-    ],
+    id: gameIds.W3,
+    date: new Date("2025-08-10"),
+    round: 3,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 12,
+      team: undefined,
+      teamScore: 56,
+      /*periodScores: [6, 23, 9, 18], // +1 to 1st*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 14,
+      team: undefined,
+      teamScore: 31, // L?
+      /*periodScores: [7, 10, 5, 9],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W4,
+    date: new Date("2025-08-17"),
+    round: 4,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 51,
+      /*periodScores: [7, 12, 14, 18], // +3 = 51*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 17,
+      team: undefined,
+      teamScore: 51,
+      /*periodScores: [12, 8, 14, 17], // -1 = 51*/
+      playerLogs: [],
+    }],
+  },
+  // W5 = bye
+  {
+    id: gameIds.W6,
+    date: new Date("2025-08-31"),
+    round: 6,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 44,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 13,
+      team: undefined,
+      teamScore: 0,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W7,
+    date: new Date("2025-09-14"),
+    round: 7,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 50,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 12,
+      team: undefined,
+      teamScore: 0,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W8,
+    date: new Date("2025-09-21"),
+    round: 8,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 55,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 15,
+      team: undefined,
+      teamScore: 0,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W9,
+    date: new Date("2025-09-28"),
+    round: 9,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 52,
+      /*periodScores: [12, 17, 12, 11],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 16,
+      team: undefined,
+      teamScore: 47, // L?
+      /*periodScores: [6, 15, 17, 9],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W10,
+    date: new Date("2025-10-12"),
+    round: 10,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 71,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 14,
+      team: undefined,
+      teamScore: 0,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W11,
+    date: new Date("2025-10-19"),
+    round: 11,
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 51,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 13,
+      team: undefined,
+      teamScore: 0,
+      /*periodScores: [0, 0, 0, 0],*/
+      playerLogs: [],
+    }],
+  },
+  {
+    id: gameIds.W12,
+    date: new Date("2025-11-02"),
+    round: 12, // semi-finals
+    teamLogs: [{
+      id: teamLogId++,
+      teamId: 11,
+      team: undefined,
+      teamScore: 47,
+      /*periodScores: [19, 5, 6, 17],*/
+      playerLogs: [],
+    }, {
+      id: teamLogId++,
+      teamId: 17,
+      team: undefined,
+      teamScore: 55,
+      /*periodScores: [12, 18, 10, 15],*/
+      playerLogs: [],
+    }],
   },
 ];
 
