61437906c655124372f251fd07e4ab5fb10b947c
Dean Hardaker <ober9k@users.noreply.github.com>
2026-03-30 23:26:04 +1100

    Added initial vite configuration

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>bball-score-react</h1>
+          <p>
+            Hello World.
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
