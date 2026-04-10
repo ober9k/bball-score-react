ecdb6188c9d26fd7dd49c96dc0da724d7074c6ff
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-11 01:14:24 +1000

    Tidied up handling of URLs in query functions

--- src/apis/query-functions.ts
+++ src/apis/query-functions.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const RootApiPath = "http://localhost/";
 const RootApiPort = "8080";
 
+
 /**
  * Build standardised URL for API usage.
  * @param parts
@@ -17,58 +18,71 @@ function buildApiPath(...parts: Array<string>): string {
 }
 
 /**
- * Build standardised URL for API usage.
+ * Build standardised URL for league API usage.
  * @param parts
  */
 export function buildUsersApiPath(...parts: Array<string>): string {
-  const url =  new URL(RootApiPath);
-  url.port = RootApiPort;
-  url.pathname = ["api", "v1", "users", ...parts].join("/");
+export function buildLeagueApiPath(...parts: Array<string>): string {
+  return buildApiPath("league", "1", ...parts);
+}
 
-  return url.toString();
+/**
+ * Build standardised URL for auth API usage.
+ * @param parts
+ */
+export function buildAuthApiPath(...parts: Array<string>): string {
+  return buildApiPath("auth", ...parts);
+}
+
+/**
+ * Build standardised URL for users API usage.
+ * @param parts
+ */
+export function buildUsersApiPath(...parts: Array<string>): string {
+  return buildApiPath("users", ...parts);
 }
 
 export async function divisionsQueryFn() {
-  const { data } = await axios.get(buildApiPath("divisions"));
+  const { data } = await axios.get(buildLeagueApiPath("divisions"));
   return data;
 }
 
 export async function divisionQueryFn({ queryKey }) {
   const [ key, divisionId ] = queryKey;
-  const { data } = await axios.get(buildApiPath("divisions", divisionId));
+  const { data } = await axios.get(buildLeagueApiPath("divisions", divisionId));
   return data;
 }
 
 export async function playersQueryFn() {
-  const { data } = await axios.get(buildApiPath("players"));
+  const { data } = await axios.get(buildLeagueApiPath("players"));
   return data;
 }
 
 export async function playerQueryFn({ queryKey }) {
   const [ key, playerId ] = queryKey;
-  const { data } = await axios.get(buildApiPath("players", playerId));
+  const { data } = await axios.get(buildLeagueApiPath("players", playerId));
   return data;
 }
 
 export async function seasonsQueryFn() {
-  const { data } = await axios.get(buildApiPath("seasons"));
+  const { data } = await axios.get(buildLeagueApiPath("seasons"));
   return data;
 }
 
 export async function seasonQueryFn({ queryKey }) {
   const [ key, seasonId ] = queryKey;
-  const { data } = await axios.get(buildApiPath("seasons", seasonId));
+  const { data } = await axios.get(buildLeagueApiPath("seasons", seasonId));
   return data;
 }
 
 export async function teamsQueryFn() {
-  const { data } = await axios.get(buildApiPath("teams"));
+  const { data } = await axios.get(buildLeagueApiPath("teams"));
   return data;
 }
 
 export async function teamQueryFn({ queryKey }) {
   const [ key, teamId ] = queryKey;
-  const { data } = await axios.get(buildApiPath("teams", teamId));
+  const { data } = await axios.get(buildLeagueApiPath("teams", teamId));
   return data;
 }
 
