343cc61d82167aaef72c2c9e2a6869117a960138
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-11 01:01:03 +1000

    Added handling for login and logout links

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,3 +1,5 @@
+import useUserContext from "@/hooks/use-user-context.ts";
+import { authPaths } from "@/routes/auth/routes.ts";
 import { divisionsPaths } from "@/routes/league/divisions/routes.ts";
 import { gamesPaths } from "@/routes/league/games/routes.ts";
 import { playersPaths } from "@/routes/league/players/routes.ts";
@@ -25,20 +27,37 @@ const links: Link[] = [
   { title: "Teams", to: teamsPaths.Teams, },
 ];
 
+const linkClasses = "[&.active]:underline [&.active]:font-bold rounded-md p-1 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white";
+const altLinkClasses = linkClasses + " " + "ml-auto";
+
 export default function Navigation() {
+  const userContext = useUserContext();
 
   const isExact = (link: Link): boolean => {
     return (link.to === leaguePaths.League);
   };
 
+  const isLoggedIn = (): boolean => {
+    return (userContext.loggedIn);
+  };
+
   return (
     <>
-      <nav className="flex gap-2 p-2">
+      <nav className="flex gap-2 p-2 bg-gray-800">
         {links.map((link, index) => (
-          <Link key={index} to={link.to} activeOptions={{ exact: isExact(link) }} className="[&.active]:underline">
+          <Link key={index} to={link.to} activeOptions={{ exact: isExact(link) }} className={linkClasses}>
             {link.title}
           </Link>
         ))}
+        {(isLoggedIn()) ? (
+          <Link to={authPaths.Logout} className={altLinkClasses}>
+            Logout
+          </Link>
+        ) : (
+          <Link to={authPaths.Login} className={altLinkClasses}>
+            Login
+          </Link>
+        )}
       </nav>
     </>
   );
