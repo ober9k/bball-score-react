50e9727393f99c3fdffc0c692ab5f993475f9f1e
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-12 17:12:08 +1000

    Added initial basic phrase handling

---
+++ src/lib/phrases.ts
@@ -0,0 +1,25 @@
+/**
+ * Base phrase set for the platform.
+ * This is applied to a map for convenience.
+ */
+const phrases = {
+  "position.type.label.guard":         "Guard",
+  "position.type.label.pointGuard":    "Point Guard",
+  "position.type.label.shootingGuard": "Shooting Guard",
+  "position.type.label.forward":       "Forward",
+  "position.type.label.smallForward":  "Small Forward",
+  "position.type.label.powerForward":  "Power Forward",
+  "position.type.label.center":        "Center",
+};
+
+const phrasesMap = new Map<string, string>(Object.entries(phrases));
+
+/**
+ * This is built as a placeholder to apply something properly later.
+ * @param key
+ */
+export const i18n = (key: string): string => {
+  return phrasesMap.has(key)
+    ? phrasesMap.get(key)
+    : "Undefined.";
+}
