ba06825f4153280cb9db40c579f75de26c5baaa8
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-15 00:19:46 +1000

    Added temporary convenience menu for manager

--- src/pages/league/league-page.tsx
+++ src/pages/league/league-page.tsx
@@ -1,10 +1,14 @@
 import { useAuthContext } from "@/auth.tsx";
 import usePageContext from "@/hooks/use-page-context.ts";
-import { useEffect } from "react";
+import { managerPaths } from "@/routes/manager/routes.ts";
+import { Button } from "@/shared/components/ui/button.tsx";
+import { useRouter } from "@tanstack/react-router";
+import { Fragment, useEffect } from "react";
 
 export default function LeaguePage() {
   const { setPageHeader } = usePageContext();
   const { user, isAuthenticated } = useAuthContext();
+  const router = useRouter();
 
   useEffect(() => {
     setPageHeader("League", "", [
@@ -12,11 +16,50 @@ export default function LeaguePage() {
     ]);
   }, []);
 
+  const gotoSeasons = () => {
+    router.navigate({
+      to: managerPaths.Seasons.Index,
+      replace: true,
+    });
+  };
+
+  const gotoDivisions = () => {
+    router.navigate({
+      to: managerPaths.Divisions.Index,
+      replace: true,
+    });
+  };
+
+  const gotoTeams = () => {
+    router.navigate({
+      to: managerPaths.Teams.Index,
+      replace: true,
+    });
+  };
+
+  const gotoPlayers = () => {
+    alert("TBD");
+  };
+
+  const gotoGames = () => {
+    alert("TBD");
+  };
+
   return (
     <>
-      <p className="p-2 text-sm">
-        TBD.
-      </p>
+      {isAuthenticated() && (
+        <Fragment>
+          <h2 className="p-2 font-medium">Manage League</h2>
+          <p className="p-2 text-sm flex gap-1">
+            <Button onClick={() => gotoSeasons()}>Seasons</Button>
+            <Button onClick={() => gotoDivisions()}>Divisions</Button>
+            <Button onClick={() => gotoTeams()}>Teams</Button>
+            <Button onClick={() => gotoPlayers()}>Players</Button>
+            <Button onClick={() => gotoGames()}>Games</Button>
+          </p>
+        </Fragment>
+      )}
+      <h2 className="p-2 font-medium">Current User</h2>
       <p className="p-2 text-sm">
         User: {isAuthenticated() ? user().email : "Not logged in..."}
       </p>
