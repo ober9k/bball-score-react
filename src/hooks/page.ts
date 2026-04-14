09f47b0e62c7da044111ae570367df9181edbba0
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-14 17:44:35 +1000

    Added utility hooks for title and breadcrumbs

---
+++ src/hooks/page.ts
@@ -0,0 +1,25 @@
+import type { Link } from "@/components/layout/page/page-breadcrumbs.tsx";
+import usePageContext from "@/hooks/use-page-context.ts";
+import { useEffect } from "react";
+
+export function useTitle(
+  title: string,
+  subTitle: string = "",
+) {
+  const { setTitle, setSubTitle } = usePageContext();
+
+  useEffect(() => {
+    setTitle(title);
+    setSubTitle(subTitle);
+  }, [title, subTitle]);
+}
+
+export function useBreadcrumbs(
+  breadcrumbs: Link[] = [],
+) {
+  const { setBreadcrumbs } = usePageContext();
+
+  useEffect(() => {
+    setBreadcrumbs(breadcrumbs);
+  }, [breadcrumbs]);
+}
