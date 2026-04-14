9e94f50752e1251d66558317042cfda6a8bfd852
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-14 23:29:40 +1000

    Added schemas for divisions and teams

---
+++ src/schemas/division.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { maxLengthMessage, minLengthMessage } from "../lib/messages.ts";
+
+const NameLabel = "Name";
+const NameMinLength = 1;
+const NameMaxLength = 20;
+
+const SeasonLabel = "Season";
+const SeasonMinLength = 1;
+
+export const zDivision = z.object({
+  name: z
+    .string()
+    .min(NameMinLength, minLengthMessage(NameLabel, NameMinLength))
+    .max(NameMaxLength, maxLengthMessage(NameLabel, NameMaxLength)),
+  seasonId: z
+    .number()
+    .min(SeasonMinLength, minLengthMessage(SeasonLabel, SeasonMinLength)),
+});
---
+++ src/schemas/team.ts
@@ -0,0 +1,25 @@
+import { maxLengthMessage, minLengthMessage } from "@/lib/messages";
+import { z } from "zod";
+
+const NameLabel = "Name";
+const NameMinLength = 1;
+const NameMaxLength = 32;
+
+const ShortNameLabel = "Short Name";
+const ShortNameMinLength = 3;
+const ShortNameMaxLength = 3;
+
+const DivisionLabel = "Division";
+const DivisionMinLength = 1;
+
+export const zTeam = z.object({
+  name: z.string()
+    .min(NameMinLength, minLengthMessage(NameLabel, NameMinLength))
+    .max(NameMaxLength, maxLengthMessage(NameLabel, NameMaxLength)),
+  shortName: z.string()
+    .min(ShortNameMinLength, minLengthMessage(ShortNameLabel, ShortNameMinLength))
+    .max(ShortNameMaxLength, maxLengthMessage(ShortNameLabel, ShortNameMaxLength)),
+  divisionId: z
+    .number()
+    .min(DivisionMinLength, minLengthMessage(DivisionLabel, DivisionMinLength)),
+});
