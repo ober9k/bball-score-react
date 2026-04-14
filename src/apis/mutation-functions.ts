cea6cd21a4328798a9d5a21d5fe28b93f9a6ea33
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-14 23:07:02 +1000

    Added shared mutation methods

--- src/apis/mutation-functions.ts
+++ src/apis/mutation-functions.ts
@@ -1,4 +1,5 @@
 import { buildAuthApiPath, buildLeagueApiPath } from "@/apis/query-functions.ts";
+import type { DivisionData } from "@/types/division.ts";
 import type { LoginData } from "@/types/login.ts";
 import type { TeamData } from "@/types/team.ts";
 import axios from "axios";
@@ -8,16 +9,40 @@ export async function loginMutationFn(loginData: LoginData) {
   return data;
 }
 
-export function buildSeasonMutationFn(seasonId?: number) {
-  const apiUrl = (!seasonId)
-    ? buildLeagueApiPath("seasons")
-    : buildLeagueApiPath("seasons", `${seasonId}`);
+export async function divisionMutationFn(divisionData: DivisionData) {
+  const { data } = await axios.put(buildLeagueApiPath("divisions", "1"), divisionData);
+  return data;
+}
+
+export async function teamMutationFn(teamData: TeamData) {
+  const { data } = await axios.put(buildLeagueApiPath("teams", "1"), teamData);
+  return data;
+}
+
+type PathKey = "seasons" | "divisions" | "teams";
+
+function buildBaseMutationFn(pathKey: PathKey, id?: number) {
+  const apiUrl = (id && id > 0)
+    ? buildLeagueApiPath(pathKey, id.toString())
+    : buildLeagueApiPath(pathKey);
 
   return async function(seasonData) {
-    const { data } = (!seasonId)
-      ? await axios.post(apiUrl, seasonData)
-      : await axios.put(apiUrl, seasonData);
+    const { data } = (id && id > 0)
+      ? await axios.put(apiUrl, seasonData)
+      : await axios.post(apiUrl, seasonData);
 
     return data;
   }
+}
+
+export function buildSeasonsMutationFn(id?: number) {
+  return buildBaseMutationFn("seasons", id);
+}
+
+export function buildDivisionsMutationFn(id?: number) {
+  return buildBaseMutationFn("divisions", id);
+}
+
+export function buildTeamsMutationFn(id?: number) {
+  return buildBaseMutationFn("teams", id);
 }
