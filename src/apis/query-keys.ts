74e1eefe9df68507557fab96ce6d23435f2c4992
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-15 13:14:05 +1000

    Added back in singular and plural keys

--- src/apis/query-keys.ts
+++ src/apis/query-keys.ts
@@ -1,34 +1,40 @@
 export const queryKeys = {
   Seasons:   "seasons",
+  Season:    "season",
   Divisions: "divisions",
+  Division:  "division",
   Teams:     "teams",
+  Team:      "team",
   Players:   "players",
+  Player:    "player",
   Games:     "games",
+  Game:      "game",
   Logout:    "logout",
 }
 
-const getQK = (baseQK: string, id?: number): Array<string|number> => {
+const getQK = (baseQK: string[], id?: number): Array<string|number> => {
+  const [ all, byId ] = baseQK;
   return (id && id > 0)
-    ? [baseQK, { id }]
-    : [baseQK];
+    ? [byId, { id }]
+    : [all];
 }
 
 export const getSeasonsQK = (id?: number): Array<string|number> => {
-  return getQK(queryKeys.Seasons, id);
+  return getQK([queryKeys.Seasons, queryKeys.Season], id);
 };
 
 export const getDivisionsQK = (id?: number): Array<string|number> => {
-  return getQK(queryKeys.Divisions, id);
+  return getQK([queryKeys.Divisions, queryKeys.Division], id);
 };
 
 export const getTeamsQK = (id?: number): Array<string|number> => {
-  return getQK(queryKeys.Teams, id);
+  return getQK([queryKeys.Teams, queryKeys.Team], id);
 }
 
 export const getPlayersQK = (id?: number): Array<string|number> => {
-  return getQK(queryKeys.Players, id);
+  return getQK([queryKeys.Players, queryKeys.Player], id);
 };
 
 export const getGamesQK = (id?: number): Array<string|number> => {
-  return getQK(queryKeys.Games, id);
+  return getQK([queryKeys.Games, queryKeys.Game], id);
 };
