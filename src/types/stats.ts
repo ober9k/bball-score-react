d9a6d57ae67916c2644b99c433396b90b61cdea8
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-16 18:13:28 +1000

    Added stats keys values

--- src/types/stats.ts
+++ src/types/stats.ts
@@ -1,3 +1,26 @@
+/**
+ * Special keys used for handling formatting and rendering.
+ */
+export const StatsKey = {
+  Minutes:              "minutes",
+  FieldGoals:           "fieldGoals",  /* all field goals */
+  TwoPointFieldGoals:   "twoPointFieldGoals",   /* only 2 point field goals */
+  ThreePointFieldGoals: "threePointFieldGoals", /* only 3 point field goals */
+  FreeThrows:           "freeThrows",
+  OffensiveRebounds:    "offensiveRebounds",
+  DefensiveRebounds:    "defensiveRebounds",
+  Rebounds:             "rebounds",
+  Assists:              "assists",
+  Steals:               "steals",
+  Blocks:               "blocks",
+  Turnovers:            "turnovers",
+  PersonalFouls:        "personalFouls",
+  TechnicalFouls:       "technicalFouls",
+  Points:               "points",
+} as const;
+
+export type StatsKeyType = typeof StatsKey[keyof typeof StatsKey];
+
 export type Stats = {
   seconds:        number,
   fgMade:         number,
