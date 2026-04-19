90bf5a3ae51390078dfa7956f8f889829649b5c0
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-19 16:27:49 +1000

    Added points diff and totals points handling

--- src/pages/league/standings-page.tsx
+++ src/pages/league/standings-page.tsx
@@ -2,16 +2,39 @@ import type { StandingsLoaderProps } from "@/apis/loaders/types.ts";
 import { useBreadcrumbs, useTitle } from "@/hooks/page.ts";
 import { leaguePaths } from "@/routes/league/routes.ts";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/shared/components/ui/table.tsx";
+import type { StandingsLog } from "@/types/standings-log.ts";
 import { getRouteApi } from "@tanstack/react-router";
 
+function getPointsDiff(log: StandingsLog): string {
+  const diff = ((log.pointsFor - log.pointsAgainst) / log.played);
+  const diffValue = diff.toFixed(1);
+
+  return (diff > 0)
+    ? `+${diffValue}`
+    : diffValue;
+}
+
+function getPoints(log: StandingsLog): number {
+  return (log.wins * 3) + (log.draws * 2) + (log.losses) + (log.byes);
+}
+
 export function StandingsPage() {
-  const { standings: standingsLogs }: StandingsLoaderProps = getRouteApi(leaguePaths.League.Standings).useLoaderData();
+  const { standings }: StandingsLoaderProps = getRouteApi(leaguePaths.League.Standings).useLoaderData();
 
   useTitle("Standings");
   useBreadcrumbs([
     { title: "League", to: leaguePaths.League.Index },
     { title: "Standings" },
   ]);
+
+  const standingsLogs = standings.sort((logA: StandingsLog, logB: StandingsLog) => {
+    const pointsA = getPoints(logA);
+    const pointsB = getPoints(logB);
+
+    if (pointsA < pointsB) return  1;
+    if (pointsA > pointsB) return -1;
+    return 0;
+  });
 
   return (
     <>
@@ -24,9 +47,8 @@ export function StandingsPage() {
             <TableHead>L</TableHead>
             <TableHead>D</TableHead>
             <TableHead>B</TableHead>
-            <TableHead>F</TableHead>
-            <TableHead>PF</TableHead>
-            <TableHead>PA</TableHead>
+            <TableHead>DIFF</TableHead>
+            <TableHead>PTS</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -38,9 +60,8 @@ export function StandingsPage() {
             <TableCell>{log.losses}</TableCell>
             <TableCell>{log.draws}</TableCell>
             <TableCell>{log.byes}</TableCell>
-            <TableCell>{log.forfeits}</TableCell>
-            <TableCell>{log.pointsFor}</TableCell>
-            <TableCell>{log.pointsAgainst}</TableCell>
+            <TableCell>{getPointsDiff(log)}</TableCell>
+            <TableCell>{getPoints(log)}</TableCell>
           </TableRow>
           ))}
         </TableBody>
