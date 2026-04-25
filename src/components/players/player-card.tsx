ff08de05cca3a95117c20cb3b2c5cfae5b3c1d4d
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-25 23:34:10 +1000

    Added basic stats to player card

--- src/components/players/player-card.tsx
+++ src/components/players/player-card.tsx
@@ -2,14 +2,22 @@ import { PlayerPosition } from "@/components/players/player-position.tsx";
 import { leaguePaths } from "@/routes/league/routes.ts";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/shared/components/ui/card";
 import type { Player } from "@/types/player.ts";
+import type { Stats } from "@/types/stats.ts";
 import { Link } from "@tanstack/react-router";
 import { Fragment } from "react";
+import * as styles from "@/components/players/player-card.module.css";
 
 type Props = {
   player: Player,
+  stats?: Stats,
 };
 
-export default function PlayerCard({ player }: Props) {
+export default function PlayerCard({ player, stats }: Props) {
+
+  const [ points, rebounds, assists ] = [ stats?.points, stats?.rebounds, stats?.assists ]
+    .map((value) => value || 0)
+    .map((value) => value.toFixed(1));
+
   return (
     <Fragment>
       <Card>
@@ -23,10 +31,21 @@ export default function PlayerCard({ player }: Props) {
         <CardContent>
           <PlayerPosition player={player} />
         </CardContent>
-        <CardFooter className="border-t-gray-200 flex justify-center gap-2">
-          <div>0.0 PTS</div>
-          <div>0.0 REB</div>
-          <div>0.0 AST</div>
+        <CardFooter className={styles.footer}>
+          <div className={styles.stats}>
+            <div>
+              <span className={styles.statsValue}>{points}</span>
+              <span className={styles.statsTitle}>Points</span>
+            </div>
+            <div>
+              <span className={styles.statsValue}>{rebounds}</span>
+              <span className={styles.statsTitle}>Rebounds</span>
+            </div>
+            <div>
+              <span className={styles.statsValue}>{assists}</span>
+              <span className={styles.statsTitle}>Assists</span>
+            </div>
+          </div>
         </CardFooter>
       </Card>
     </Fragment>
