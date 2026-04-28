618d59661b03007fdd096b33048b4d747c0ee5b5
Dean Hardaker <ober9k@users.noreply.github.com>
2026-04-29 00:44:11 +1000

    Tided up loader return types to remove compilation warnings

--- src/apis/loaders.ts
+++ src/apis/loaders.ts
@@ -35,43 +35,43 @@ import type { StandingsLog } from "@/types/standings-log.ts";
 import type { StatisticsLog } from "@/types/statistics-log.ts";
 import type { Team } from "@/types/team.ts";
 
-export async function seasonsLoader({ context }): SeasonsLoaderProps {
+export async function seasonsLoader({ context }): Promise<SeasonsLoaderProps> {
   return {
     seasons: await fetchAll<Season>(context.queryClient, buildSeasonsQueryOptions()),
   }
 }
 
-export async function seasonLoader({ context, params }): SeasonLoaderProps {
+export async function seasonLoader({ context, params }): Promise<SeasonLoaderProps> {
   return {
     season: await fetchById<Season>(context.queryClient, buildSeasonsQueryOptions(+params.seasonId)),
   }
 }
 
-export async function divisionsLoader({ context }): DivisionsLoaderProps {
+export async function divisionsLoader({ context }): Promise<DivisionsLoaderProps> {
   return {
     divisions: await fetchAll<Division>(context.queryClient, buildDivisionsQueryOptions()),
   }
 }
 
-export async function divisionLoader({ context, params }): DivisionLoaderProps {
+export async function divisionLoader({ context, params }): Promise<DivisionLoaderProps> {
   return {
     division: await fetchById<Division>(context.queryClient, buildDivisionsQueryOptions(+params.divisionId)),
   }
 }
 
-export async function teamsLoader({ context }): TeamsLoaderProps {
+export async function teamsLoader({ context }): Promise<TeamsLoaderProps> {
   return {
     teams: await fetchAll<Team>(context.queryClient, buildTeamsQueryOptions()),
   }
 }
 
-export async function teamLoader({ context, params }): TeamLoaderProps {
+export async function teamLoader({ context, params }): Promise<TeamLoaderProps> {
   return {
     team: await fetchById<Team>(context.queryClient, buildTeamsQueryOptions(+params.teamId)),
   }
 }
 
-export async function teamPlayersLoader({ context, params }): TeamPlayersLoaderProps {
+export async function teamPlayersLoader({ context, params }): Promise<TeamPlayersLoaderProps> {
   return {
     team:           await fetchById<Team>(context.queryClient, buildTeamsQueryOptions(+params.teamId)),
     players:        await fetchById<Player>(context.queryClient, buildTeamPlayersQueryOptions(+params.teamId)),
@@ -79,20 +79,20 @@ export async function teamPlayersLoader({ context, params }): TeamPlayersLoaderP
   }
 }
 
-export async function playersLoader({ context }): PlayersLoaderProps {
+export async function playersLoader({ context }): Promise<PlayersLoaderProps> {
   return {
     players:        await fetchAll<Player>(context.queryClient, buildPlayersQueryOptions()),
     statisticsLogs: await fetchAll<StatisticsLog>(context.queryClient, buildStatisticsQueryOptions("averages")), /* todo: optimize */
   }
 }
 
-export async function playerLoader({ context, params }): PlayerLoaderProps {
+export async function playerLoader({ context, params }): Promise<PlayerLoaderProps> {
   return {
     player: await fetchById<Player>(context.queryClient, buildPlayersQueryOptions(+params.playerId)),
   }
 }
 
-export async function playerStatisticsLoader({ context, params }): PlayerStatisticsLoaderProps {
+export async function playerStatisticsLoader({ context, params }): Promise<PlayerStatisticsLoaderProps> {
   return {
     player:                 await fetchById<Player>(context.queryClient, buildPlayersQueryOptions(+params.playerId)),
     averagesStatisticsLogs: await fetchAll<StatisticsLog>(context.queryClient, buildPlayersStatisticsQueryOptions(+params.playerId, "averages")), /* todo: optimize */
@@ -100,20 +100,20 @@ export async function playerStatisticsLoader({ context, params }): PlayerStatist
   }
 }
 
-export async function playerGamesLoader({ context, params }): PlayerGamesLoaderProps {
+export async function playerGamesLoader({ context, params }): Promise<PlayerGamesLoaderProps> {
   return {
     player:              await fetchById<Player>(context.queryClient, buildPlayersQueryOptions(+params.playerId)),
     gamesStatisticsLogs: await fetchAll<StatisticsLog>(context.queryClient, buildPlayersStatisticsQueryOptions(+params.playerId, "games")),   /* todo: optimize */
   }
 }
 
-export async function gamesLoader({ context }): GamesLoaderProps {
+export async function gamesLoader({ context }): Promise<GamesLoaderProps> {
   return {
     games: await fetchAll<Game>(context.queryClient, buildGamesQueryOptions()),
   }
 }
 
-export async function gameLoader({ context, params }): GameLoaderProps {
+export async function gameLoader({ context, params }): Promise<GameLoaderProps> {
   return {
     game: await fetchById<Game>(context.queryClient, buildGamesQueryOptions(+params.gameId)),
   }
